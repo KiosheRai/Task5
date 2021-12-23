20fe7269db1d9e01f531466c4aa3d7144bda729a
Adler <prop.het@mail.ru>
2021-12-23 18:24:06 +0300

    Добавьте файлы проекта.

---
+++ Task5/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Task5.Models;
+
+namespace Task5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return Content(User.Identity.Name);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Task5/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task5.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public DateTime LastLoginDate { get; set; }
+        public string Status { get; set; }
+    }
+}
