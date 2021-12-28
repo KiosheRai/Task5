96a0bb48b0245a48b1bcf6b3c4cbd2d4f367bd36
Adler <prop.het@mail.ru>
2021-12-28 19:06:11 +0300

    V1.5

--- Task5/Controllers/UserPanelController.cs
+++ Task5/Controllers/UserPanelController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using System.Linq;
 using System.Threading.Tasks;
 using Task5.Models;
 
@@ -52,6 +53,80 @@ namespace Task5.Controllers
             await db.SaveChangesAsync();
 
             return RedirectToAction("Index", "UserPanel");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListOfMessages()
+        {
+            var List = await db.Messages.Where(a => a.Recipient == User.Identity.Name).ToListAsync();
+
+            if(List != null)
+            {
+                return View(List);
+                
+            }
+            ViewData["Count"] = "Список входящих сообщений пуст.";
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(string[] list, string action)
+        {
+            if (action == "Delete")
+            {
+                return await Delete(list);
+            }
+            else if (action == "Check")
+            {
+                return await Check(list);
+            }
+            else
+            {
+                return await UnCheck(list);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string[] list)
+        {
+            foreach (var x in list)
+            {
+                TextMessage message = await db.Messages.FirstOrDefaultAsync(p => p.id.ToString() == x);
+
+                db.Messages.Remove(message);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ListOfMessages");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Check(string[] list)
+        {
+            foreach (var x in list)
+            {
+                TextMessage message = await db.Messages.FirstOrDefaultAsync(p => p.id.ToString() == x);
+
+                message.IsChecked = true;
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ListOfMessages");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UnCheck(string[] list)
+        {
+            foreach (var x in list)
+            {
+                TextMessage message = await db.Messages.FirstOrDefaultAsync(p => p.id.ToString() == x);
+
+                message.IsChecked = false;
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ListOfMessages");
         }
     }
 }
